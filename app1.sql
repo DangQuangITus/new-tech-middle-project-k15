2026282b5d25797a77fde1fd539215dd9a3ba300
quangnd2 <nguyendangquangkt@gmail.com>
2018-11-17 17:44:17 +0700

    update DB

--- app1.sql
+++ app1.sql
@@ -2,9 +2,9 @@ CREATE DATABASE  IF NOT EXISTS `app1` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `app1`;
 -- MySQL dump 10.13  Distrib 5.7.24, for Linux (x86_64)
 --
--- Host: localhost    Database: app1
+-- Host: 127.0.0.1    Database: app1
 -- ------------------------------------------------------
--- Server version	5.7.24-0ubuntu0.16.04.1
+-- Server version	5.7.24-0ubuntu0.18.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -43,8 +43,38 @@ CREATE TABLE `customer` (
 
 LOCK TABLES `customer` WRITE;
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-INSERT INTO `customer` VALUES (1,'phuong','Ha Noi','0123456789','123/8 Le hong phong','not locate',NULL,NULL),(2,'quang','Ho chi minh','0123456789','Le hong phong','not locate',NULL,NULL),(10,'nguyen Dang quang','144 Âu cơ, tân bình, Hồ chí minh','0396424817','','not locate',NULL,NULL),(11,'','','','','not located',NULL,'2018-11-10 00:00:00');
+INSERT INTO `customer` VALUES (1,'phuong','Ha Noi','0123456789','123/8 Le hong phong','located',NULL,NULL),(2,'quang','Ho chi minh','0123456789','Le hong phong','located',NULL,NULL),(10,'nguyen Dang quang','144 Âu cơ, tân bình, Hồ chí minh','0396424817','','located',NULL,NULL),(11,'','','','','located',NULL,'2018-11-10 00:00:00');
 /*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `driver`
+--
+
+DROP TABLE IF EXISTS `driver`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `driver` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `phone` decimal(10,0) NOT NULL,
+  `location` varchar(100) DEFAULT NULL,
+  `status` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `driver`
+--
+
+LOCK TABLES `driver` WRITE;
+/*!40000 ALTER TABLE `driver` DISABLE KEYS */;
+INSERT INTO `driver` VALUES (1,'quang','2b10351253eed030812674e8aa18a5ab','Quang','Dang',133456,'{\"lat\":10.796236799999999,\"lng\":106.6819584}','undefined'),(2,'quang','2b10351253eed030812674e8aa18a5ab','Quang','Dang',123,NULL,'disable');
+/*!40000 ALTER TABLE `driver` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -64,4 +94,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-10 20:04:43
+-- Dump completed on 2018-11-17 15:58:38
